9c85f377d6b2b2416125ee16266be60bde13a913
Aditya Kanthe <adityakanthe10@gmail.com>
2023-12-11 16:53:53 +0530

    basic javascript building done

--- 4.Odin_Rock Paper Scissors/script.js
+++ 4.Odin_Rock Paper Scissors/script.js
@@ -1,28 +1,41 @@
 // Javascript Rock Paper Scissors
-function playGame(playerChoice) {
-  var choices = ["rock", "paper", "scissors"];
-  var computerChoice = choices[Math.floor(Math.random() * 3)];
+// create a function getComputerChoice  that will return Rock Paper Scissors,
+// getComputerChoice is the function that makes the computer play
+// creater a function playRound that takes two parameters playerSelection and
+// computerSelection and returns a string that declares the winner of the round
+// return- "You Lose! Paper beats Rock"
+// users input must be case-insensitive
+// ties should be accounted by replaying round.
 
-  var result = determineWinner(playerChoice, computerChoice);
-
-  displayResult(playerChoice, computerChoice, result);
-}
-
-function determineWinner(playerChoice, computerChoice) {
-  if (playerChoice === computerChoice) {
-    return "It's a tie!";
-  } else if (
-    (playerChoice === "rock" && computerChoice === "scissors") ||
-    (playerChoice === "paper" && computerChoice === "rock") ||
-    (playerChoice === "scissors" && computerChoice === "paper")
-  ) {
-    return "You win!";
-  } else {
-    return "You lose!";
+function getComputerChoice() {
+  let randomChoice = Math.floor(Math.random() * 3);
+  if (randomChoice === 0) {
+    return "rock";
+  } else if (randomChoice === 1) {
+    return "scissors";
+  } else if (randomChoice === 2) {
+    return "paper";
   }
 }
 
-function displayResult(playerChoice, computerChoice, result) {
-  var message = `You chose ${playerChoice}, the computer chose ${computerChoice}. ${result}`;
-  document.getElementById("result").innerText = message;
+function playRound(playerSelection, computerSelection) {
+  const playerChoice = playerSelection.toLowerCase();
+
+  if (playerChoice === computerSelection) {
+    return "It's a TIE!!!";
+  } else if (
+    (playerChoice === "rock" && computerSelection === "scissors") ||
+    (playerChoice === "paper" && computerSelection === "rock") ||
+    (playerChoice === "scissors" && computerSelection === "rock")
+  ) {
+    return `You win! ${playerChoice} beats ${computerSelection} `;
+  } else {
+    return ` You Lose! ${computerSelection} beats ${playerChoice}`;
+  }
 }
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+//  Write a NEW function called game(). Use the previous function inside of this one to play a best-of-five game that keeps score and reports a winner or loser at the end.
