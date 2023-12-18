5a9abc64478299ec71c934fc571e16fbd229ae99
Aditya Kanthe <adityakanthe10@gmail.com>
2023-12-18 17:29:20 +0530

    completed fundamental4,5 exercisee

--- 3.Odin_Rock Paper Scissors/Fundamental4_js_exe/javascript-exercises-main/javascript-exercises-main/09_palindromes/palindromes.js
+++ 3.Odin_Rock Paper Scissors/Fundamental4_js_exe/javascript-exercises-main/javascript-exercises-main/09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const processsedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+  return processsedString.split("").reverse().join("") == processsedString;
 };
 
 // Do not edit below this line
