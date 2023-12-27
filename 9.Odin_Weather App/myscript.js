c03aa712f10e7d009e301054e82590cc24f4e2c7
Aditya Kanthe <adityakanthe10@gmail.com>
2023-12-27 16:42:13 +0530

    html and woking on css halfdone started react

---
+++ 9.Odin_Weather App/myscript.js
@@ -0,0 +1,15 @@
+// Set up a blank HTML document with the appropriate links to your JavaScript and CSS files.
+// Write the functions that hit the API. You’re going to want functions that can take a location and return the weather data for that location. For now, just console.log() the information.
+// Write the functions that process the JSON data you’re getting from the API and return an object with only the data you require for your app.
+// Set up a simple form that will let users input their location and will fetch the weather info (still just console.log() it).
+// Display the information on your webpage!
+// Add any styling you like!
+// Optional: add a ‘loading’ component that displays from the time the form is submitted until the information comes back from the API. Use DevTools to test for low-end devices.
+// Push that baby to github and share your solution below!
+
+// 1. fetch details from api make
+// 2.Front-end
+// _basic :1.search city,Date,Time,temp,humidity,wind speed,chance of rain
+// _medium : 2.add to css - logos,images
+// 3.Add a feature that tells the weather forecast of the next 7 days(daily)
+// 4.Add same feature for hourly
