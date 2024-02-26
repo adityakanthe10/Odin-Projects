37577f52fb877b6c6995d202afc296298078bcfe
Aditya Kanthe <adityakanthe10@gmail.com>
2024-02-26 19:44:19 +0530

    styling webpage done

---
+++ 6.Odin_Signup_Form/script.js
@@ -0,0 +1,11 @@
+document.adddEventListener("DOMContentLoaded", function () {
+  const firstname = document.querySelector("#firstname");
+  const firstnameError = document.querySelector("#first-name-error");
+  firstname.addEventListener("input", function (event) {
+    if (!firstname.value.trim() === "") {
+      firstnameError.textContent = "Please Type in your first name.";
+    } else {
+      firstnameError.textContent = "";
+    }
+  });
+});
