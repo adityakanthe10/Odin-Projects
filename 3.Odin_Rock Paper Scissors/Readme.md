6f79478f89b1387d5625ebe4312e64f1991dff85
Aditya Kanthe <adityakanthe10@gmail.com>
2024-03-12 20:00:50 +0530

    to-do ux

--- 3.Odin_Rock Paper Scissors/Readme.md
+++ 3.Odin_Rock Paper Scissors/Readme.md
@@ -2,5 +2,8 @@
 
 # Todo
 
-- Add DOM in webpage
-- Add CSS Style
+1. Create A  Game Function That would Keep the Best of five Result
+2. Use Dom -
+    a. Create Final Result that would be best of five .
+    b. Update score of players, computer  and rounds played .
+    c. Use Images instead of buttons.
--- 3.Odin_Rock Paper Scissors/script.js
+++ 3.Odin_Rock Paper Scissors/script.js
@@ -1,12 +1,34 @@
-// Javascript Rock Paper Scissors
-// create a function getComputerChoice  that will return Rock Paper Scissors,
-// getComputerChoice is the function that makes the computer play
-// creater a function playRound that takes two parameters playerSelection and
-// computerSelection and returns a string that declares the winner of the round
-// return- "You Lose! Paper beats Rock"
-// users input must be case-insensitive
-// ties should be accounted by replaying round.
+//  DOM to take input .
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissor = document.getElementById("scissor");
 
+rock.addEventListener("click", function () {
+  inputChoice("rock");
+});
+paper.addEventListener("click", function () {
+  inputChoice("paper");
+});
+scissor.addEventListener("click", function () {
+  inputChoice("scissor");
+});
+
+function inputChoice(choice) {
+  playerSelection = choice;
+  computerSelection = getComputerChoice();
+  const result = playRound(playerSelection, computerSelection);
+  const roundScoreDiv = document.getElementById("roundScore");
+  if (roundScoreDiv) {
+    roundScoreDiv.textContent = result.meassage;
+  } else {
+    console.error("roundScoreDiv is null.Element not found");
+  }
+}
+// Introduced choices
+let playerSelection;
+let computerSelection;
+
+// Random Computer Choice.
 function getComputerChoice() {
   let randomChoice = Math.floor(Math.random() * 3);
   if (randomChoice === 0) {
@@ -17,40 +39,44 @@ function getComputerChoice() {
     return "paper";
   }
 }
-// const inputChoice = prompt("Rock Paper Scissor");
-// playerSelection = inputChoice;
-const computerSelection = getComputerChoice();
 
-//  Write a NEW function called game(). Use the previous function inside of this one to play a best-of-five game that keeps score and reports a winner or loser at the end.
+// Introducing variables to cound round,Wins .
 
-document.getElementById("rock").onclick = function () {
-  inputChoice("rock");
-};
-document.getElementById("paper").onclick = function () {
-  inputChoice("paper");
-};
-document.getElementById("scissor").onclick = function () {
-  inputChoice("scissor");
-};
-function inputChoice(choice) {
-  playerSelection = choice;
-
-  function playRound(playerSelection, computerSelection) {
-    const playerChoice = playerSelection.toLowerCase();
-
-    if (playerChoice === computerSelection) {
-      return "It's a TIE!!!";
-    } else if (
-      (playerChoice === "rock" && computerSelection === "scissor") ||
-      (playerChoice === "paper" && computerSelection === "rock") ||
-      (playerChoice === "scissor" && computerSelection === "paper")
-    ) {
-      return `You win! ${playerChoice} beats ${computerSelection} `;
-    } else {
-      return ` You Lose! ${computerSelection} beats ${playerChoice}`;
-    }
+// Actual Logic  of per round Winnnwe
+function playRound(playerSelection, computerSelection) {
+  let computerWin = 0;
+  let rounds = 0;
+  let playerWin = 0;
+  const playerChoice = playerSelection.toLowerCase();
+  if (playerChoice === computerSelection) {
+    return {
+      message: "It's a TIE!!!",
+      playerWin,
+      computerWin,
+      rounds,
+    };
+  } else if (
+    (playerChoice === "rock" && computerSelection === "scissor") ||
+    (playerChoice === "paper" && computerSelection === "rock") ||
+    (playerChoice === "scissor" && computerSelection === "paper")
+  ) {
+    rounds++;
+    playerWin++;
+    return {
+      message: `You win! ${playerChoice} beats ${computerSelection} `,
+      rounds,
+      playerWin,
+    };
+  } else {
+    rounds++;
+    computerWin++;
+    return {
+      message: ` You Lose! ${computerSelection} beats ${playerChoice}`,
+      rounds,
+      computerWin,
+    };
   }
-  console.log(playRound(playerSelection, computerSelection));
 }
 
-const round = document.getElementById("round-score");
+//  Write a NEW function called game(). Use the previous function inside of this one to play a best-of-five game that keeps score and reports a winner or loser at the end.
+function game(playerSelection) {}
